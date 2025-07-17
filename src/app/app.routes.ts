8ef7e6c700defc02425d2177458436b8ddb059c6
John Jacko <jljacko@outlook.com>
2025-07-16 20:03:16 -0600

    Set up Basic App

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from '@tc/tc-ngx-general';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+     {path: "Logon", component: LoginComponent},
+];
---
+++ src/app/components/top-bar/top-bar.component.ts
@@ -0,0 +1,56 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { AuthService, NavBarComponent, NavClickDetails, NavOption, NavOptionShow } from '@tc/tc-ngx-general';
+import { BrandSearcherComponent } from '../brand-searcher/brand-searcher.component';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-top-bar',
+  imports: [CommonModule, NavBarComponent, BrandSearcherComponent],
+  templateUrl: './top-bar.component.html',
+  styleUrl: './top-bar.component.css'
+})
+export class TopBarComponent {
+  authService: AuthService;
+
+  navOptions: NavOption[];
+
+  constructor(authService: AuthService, private router:Router){
+    this.authService = authService;
+
+    this.navOptions = [
+      {
+        displayText: 'Welcome',
+        title: 'Welcome',
+        showOption: NavOptionShow.BASIC_DESKTOP
+      },
+      {
+        displayText: 'Factchecks',
+        title: 'factchecks-search',
+        showOption: NavOptionShow.BASIC_DESKTOP
+      },
+      {
+        displayText: 'Falsehoods',
+        title: 'falsehoods-search',
+        showOption: NavOptionShow.BASIC_DESKTOP
+      }
+      // ,
+      // {
+      //   displayText: 'Welcome',
+      //   title: 'Welcome',
+      //   showOption: NavOptionShow.BASIC_DESKTOP
+      // }
+    ]
+  }
+
+
+
+  onNavigate(details: NavClickDetails){
+    this.router.navigateByUrl('/' + (details.navLink || details.title));
+  }
+
+  prepLogin(){
+    this.router.navigateByUrl('/Logon')
+  }
+}
